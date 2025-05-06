fe9f08eb01c37bc1f50d7b73f64632d056c0c707
NamanpreetG <56648597+NamanpreetG@users.noreply.github.com>
2025-05-06 23:54:23 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+
+// Add your React code here (same as you already have in `ShaadiConnectHome` component)
+
+render(<ShaadiConnectHome />, document.getElementById('root'));
